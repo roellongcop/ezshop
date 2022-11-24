3e1bf26587890e4c37cc5006e90e127f4133d5e4
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2022-11-24 11:21:48 +0800

    Update MailerComponent.php

--- components/MailerComponent.php
+++ components/MailerComponent.php
@@ -4,6 +4,20 @@ namespace app\components;
 
 class MailerComponent extends \yii\symfonymailer\Mailer
 {
+   const TRANSPORT = [
+		// LIVE
+		'scheme' => 'smtps',
+		'host' => 'gennakar.accessgov.ph',
+		'username' => '',
+		'password' => '',
+		'port' => 465,
+	];
+
     public $useFileTransport = true;
-   
+
+	// public function init()
+	// {
+	// 	parent::init();
+	// 	$this->setTransport(self::TRANSPORT);
+	// }
 }
