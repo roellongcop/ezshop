66860c8cdb0ebd82900da3f2bfcca2a8c0514ee5
roellongcop <roel.filweb@gmail.com>
2022-11-26 21:17:52 +0800

    delete cart
    delete cart table
    
    c

--- models/form/CartForm.php
+++ models/form/CartForm.php
@@ -2,57 +2,98 @@
 
 namespace app\models\form;
 
-use app\models\Cart;
+use Yii;
+use app\helpers\ArrayHelper;
 
 class CartForm extends \yii\base\Model
 {
     public $product_id;
-    public $user_id;
     public $quantity;
     public $color;
     public $size;
 
+    public $data;
+    public $session;
+
     public function rules()
     {
         return [
-            [['product_id', 'user_id', 'quantity'], 'required'],
-            [['product_id', 'user_id', 'quantity'], 'integer'],
+            [['product_id', 'quantity'], 'required'],
+            [['product_id', 'quantity'], 'integer'],
             [['color', 'size'], 'string', 'max' => 255],
         ];
     }
 
+    public function init()
+    {
+        parent::init();
+        $this->session = Yii::$app->session;
+
+        $this->data = $this->session['cart'] ?? [];
+    }
+
+
+    public function addNewProduct()
+    {
+        $this->data[] = [
+            'product_id' => $this->quantity,
+            'quantity' => $this->quantity,
+            'color' => $this->color,
+            'size' => $this->size,
+        ];
+
+        $this->session['cart'] = $this->data;
+    }
 
     public function save()
     {
         if ($this->validate()) {
-            $condition = [
-                'product_id' => $this->product_id,
-                'user_id' => $this->user_id,
-            ];
+            if ($this->data) {
 
-            if ($this->color) {
-                $condition['color'] = $this->color;
-            }
+                $exist = false;
+                foreach ($this->data as &$data) {
+                    if ($data['product_id'] == $this->product_id) {
+                        if ($this->color && $this->size) {
+                            if ($data['color'] == $this->color && $data['size'] == $this->size) {
+                                $data['quantity'] = $data['quantity'] + $this->quantity;
+                                $exist = true;
+                                break;
+                            }
+                        }
+                        elseif ($this->color && $this->size == null) {
+                            if ($data['color'] == $this->color) {
+                                $data['quantity'] = $data['quantity'] + $this->quantity;
+                                $exist = true;
+                                break;
+                            }
+                        }
+                        elseif ($this->size && $this->color == null) {
+                            if ($data['size'] == $this->size) {
+                                $data['quantity'] = $data['quantity'] + $this->quantity;
+                                $exist = true;
+                                break;
+                            }
+                        }
+                        else {
+                            $data['quantity'] = $data['quantity'] + $this->quantity;
+                            $exist = true;
+                            break;
+                        }
+                    }
+                }
 
-            if ($this->size) {
-                $condition['size'] = $this->size;
-            }
-
-            $cart = Cart::findOne($condition) ?: new Cart($condition);
-
-            if ($cart->isNewRecord) {
-                $cart->quantity = $this->quantity;
+                if ($exist) {
+                    $this->session['cart'] = $this->data;
+                }
+                else {
+                    $this->addNewProduct();
+                }
             }
             else {
-                $cart->quantity = $cart->quantity + $this->quantity;
+                $this->addNewProduct();
             }
 
-            if ($cart->save()) {
-                return $cart;
-            }
-
-
-            $this->addError('cart', $cart->errors);
+            return $this->data;
         }
     }
 }
--- tests/unit/models/form/CartFormTest.php
+++ tests/unit/models/form/CartFormTest.php
@@ -3,14 +3,21 @@
 namespace tests\unit\models;
 
 use app\models\form\CartForm;
+use Yii;
 
 class CartFormTest extends \Codeception\Test\Unit
 {
+
+    public function _before()
+    {
+        $session = Yii::$app->session;
+
+        $session->destroy();
+    }
     protected function data($replace=[])
     {
         return array_replace([
             'product_id' => 1,
-            'user_id' => 1,
             'color' => 'Color',
             'size' => 'Size',
             'quantity' => 1,
@@ -18,12 +25,39 @@ class CartFormTest extends \Codeception\Test\Unit
     }
 
    
-    public function testCreateSuccess()
+    public function testCreateNewSuccess()
     {
         $model = new CartForm($this->data());
         expect_that($model->save());
     }
 
+    public function testCreateNewColorSuccess()
+    {
+        $model = new CartForm($this->data());
+        expect_that($model->save());
+
+        $model = new CartForm($this->data(['color' => 'new color']));
+        expect_that($model->save());
+
+        $CartForm = new CartForm();
+        expect($CartForm->data[0]['quantity'])->equals(1);
+        expect($CartForm->data[1]['color'])->equals('new color');
+    }
+
+
+    public function testCreateNewSizeSuccess()
+    {
+        $model = new CartForm($this->data());
+        expect_that($model->save());
+
+        $model = new CartForm($this->data(['size' => 'new size']));
+        expect_that($model->save());
+
+        $CartForm = new CartForm();
+        var_dump($CartForm->data); die;
+        expect($CartForm->data[0]['quantity'])->equals(2);
+        expect($CartForm->data[1]['size'])->equals('new size');
+    }
 
     public function testInvalidProductId()
     {
@@ -34,29 +68,16 @@ class CartFormTest extends \Codeception\Test\Unit
         expect($model->errors)->hasKey('product_id');
     }
 
-    public function testInvalidUserId()
-    {
-        $model = new CartForm($this->data([
-            'user_id' => 'invalid'
-        ]));
-        expect_not($model->save());
-        expect($model->errors)->hasKey('user_id');
-    }
-
     public function testAddQuantity()
     {
-        $model = new CartForm($this->data(['user_id' => 1]));
-
+        $model = new CartForm($this->data());
         expect_that($model->save());
 
-        $cart = $this->tester->grabRecord('app\models\Cart', [
-            'product_id' => 1,
-            'user_id' => 1,
-            'color' => 'Color',
-            'size' => 'Size',
-        ]);
+        $model = new CartForm($this->data());
+        expect_that($model->save());
 
-        expect($cart->quantity)->equals(2);
+        $CartForm = new CartForm();
+        expect($CartForm->data[0]['quantity'])->equals(2);
     }
 
 }
