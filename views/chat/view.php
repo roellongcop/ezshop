12cf2d941b2d1d8e99dc46a63d08c704439c016b
roellongcop <roel.filweb@gmail.com>
2022-12-02 22:14:09 +0800

    Update view.php

--- views/chat/view.php
+++ views/chat/view.php
@@ -22,7 +22,7 @@ $this->params['showCreateButton'] = true;
     	'model' => $model
     ]) ?> 
 
-    <?= App::if($model->status == Chat::UN_ANSWERED, 
+    <?= App::if($model->status != Chat::ANSWERED, 
         Html::a('Train', ['train', 'id' => $model->id], [
             'class' => 'btn btn-success font-weight-bold'
         ])
