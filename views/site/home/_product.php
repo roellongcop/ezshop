b49881e8cf0edd57e3c9c7246a08b4aa04c8de19
roellongcop <longcoproel@gmail.com>
2023-01-03 00:13:07 +0800

    Update _product.php

--- views/site/home/_product.php
+++ views/site/home/_product.php
@@ -15,7 +15,7 @@ $wishlist = in_array($product->id, $this->params['wishlistProductIds']);
         ])) ?>
 
     <div class="product-img position-relative overflow-hidden">
-        <img class="img-fluid w-100" src="<?= $product->getImageUrl(326) ?>" alt="">
+        <img class="img-fluid w-100" src="<?= $product->getImageUrl(500) ?>" alt="">
         <div class="product-action">
             <a data-product_id="<?= $product->id ?>" title="Add to Cart" data-toggle="tooltip" class="btn-add-to-cart btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
             <a title="<?= $wishlist ? 'Remove from Wishlist': 'Add to Wishlist' ?>" data-toggle="tooltip" data-product_id="<?= $product->id ?>" class="btn-add-to-wishlist btn btn-outline-dark btn-square" href="#">
