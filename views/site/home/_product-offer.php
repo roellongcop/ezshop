8f03229669ac0ad870ca0e32302ce32e49512336
roellongcop <longcoproel@gmail.com>
2023-02-05 11:29:45 +0800

    Update _product-offer.php

--- views/site/home/_product-offer.php
+++ views/site/home/_product-offer.php
@@ -1,5 +1,5 @@
 <div class="product-offer mb-30" style="height: <?= $height ?? '300px;' ?>;">
-    <img class="img-fluid" src="<?= $product->productCategoryImageUrl ?>" alt="">
+    <img class="img-fluid" src="<?= $product->getImageUrl(500) ?>" alt="">
     <div class="offer-text">
         <h6 class="text-white text-uppercase">Save <?= $product->salePercentage ?>%</h6>
         <h3 class="text-white mb-3">Special Offer</h3>
